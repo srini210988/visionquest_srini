26b2ec56680ebb3071b288596c55da68c838b67c
srini210988 <sssri19@gmail.com>
2025-02-02 13:40:59 +0530

    removed streak component code

--- src/app/streak/page.js
+++ src/app/streak/page.js
@@ -1,16 +1,13 @@
 import { Button } from "@/components/ui/button"
 import Layout from '../components/layout';
 import { Loader2, RefreshCw, Circle, Spinner } from 'lucide-react'
-import StatisticsCard from '../components/statics-card' 
-import { ProgressProvider } from "../context/progress-provider";
 export default function Page() {
     return (<Layout> 
-        <ProgressProvider>
         <div className="flex justify-center items-center"> 
             <div> 
-            <StatisticsCard />
+            progress
             </div>
         </div>
-        </ProgressProvider>
+        
     </Layout>);
 }
