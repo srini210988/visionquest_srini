f28eebab3c4439e6b1ebbbaf769596ad77f08bdd
srini210988 <sssri19@gmail.com>
2025-02-08 17:54:04 +0530

    file operation

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -1,14 +1,19 @@
+'use client'
 import Layout from '../components/layout';
 import Tabs from "../components/tabs"
 import ListComponent, { CompactListComponent } from '../components/list-component'
 import HomeBannerCarousel from '../components/home-banner' 
+import fileOperations from '../components/file-handler'
 export const viewport = {
     width : 'device-width', 
     initialScale:1,
     maximumScale:1,
     userScalable:false
   } 
-export default function Page() { 
+export default function Page() {
+    console.log("before file write"); 
+    fileOperations();
+    console.log("after file write");
     const tabContent = [{
         label : 'New',
         content : (<><ListComponent heading="Ongoing Activities"/></>)
