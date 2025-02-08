4cbf81a2264200a146a71dd115f94161c21cdfce
srini210988 <sssri19@gmail.com>
2025-02-08 18:37:31 +0530

    Update page.js

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -10,12 +10,10 @@ import { Moon, MoonIcon, Sun, BellDot, View } from "lucide-react"
 import { useActionState } from 'react';
 import { fadeInEffect } from '../components/fadein'
 import Image from "next/image"
-import { GoogleOAuthProvider,GoogleLogin} from "@react-oauth/google";
-import fileOperations from "../components/file-handler"
+import { GoogleOAuthProvider,GoogleLogin} from "@react-oauth/google"; 
 
 export default function Login() { 
-  console.log("Current working directory:", process.cwd());
-  fileOperations();
+  console.log("Current working directory:", process.cwd()); 
   console.log("file operation done");
   const router = useRouter();
   const [user, setUser] = useState(null);
